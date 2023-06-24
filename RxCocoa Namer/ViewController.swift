64f7754a33a96b1af6e0497b3b412103bfbe5262
Carrington <Manyuchi>
2023-06-24 20:23:41 +0200

    add changes

--- RxCocoa Namer/ViewController.swift
+++ RxCocoa Namer/ViewController.swift
@@ -7,12 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import RxSwift
+import RxCocoa
 
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloLbl: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    @IBOutlet weak var namesLbl: UILabel!
+    
+    let disposeBag = DisposeBag()
+    
+ 
+    
+    //var namesArray = Variable([])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bind()
     }
+    
+    func bind() {
+        nameEntryTextField.rx.text.debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+                    .map {
+                        if $0 == "" {
+                            return "Type your name below"
+                        } else {
+                           return
+                            "Hello, \($0!)."
+                        }
+        
+                    }
+                    .bind(to: helloLbl.rx.text)
+                    .disposed(by: disposeBag)
+            }
+    
 
 
 }
